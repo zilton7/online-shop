7d575e08e4ea68cb9fcfbba55de2c0357b58269f
Zil Norvilis <zilasino27@gmail.com>
2021-03-06 15:02:51 +0100

    Turn footer and header into partials and insert them into both pages

---
+++ includes/_footer.js
@@ -0,0 +1,81 @@
+let page = `
+<footer>
+        <div class="container-fluid px-5">
+            <div class="row py-4">
+                <!-- About -->
+                <div class="wrapper mx-auto col-6">
+                    <div class="d-flex justify-content-center align-items-center">
+                        <i class="fas fa-book main_logo text_color_2 pr-4 mb-2"></i>
+                        <p class="font-weight-bold h1 text_color_2">BOOKIX</p>
+                    </div>
+                    <div class="col-8 mx-auto">
+                        <p class="h5 text-muted text-justify">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora aliquid facere ipsum nam
+                            repudiandae
+                            eaque
+                            culpa repellat a numquam, ullam, at animi. Soluta itaque ipsam reiciendis hic accusantium
+                            iure alias?
+                        </p>
+                    </div>
+                </div>
+                <!-- Genres Links -->
+                <div class=" mx-auto col-3">
+                    <div class="border_color_1 mb-2">
+                        <p class="text_color_1 font-weight-bold h4">GENRES</p>
+                    </div>
+                    <div class="category_links d-block d-md-flex justify-content-start">
+                        <ul class="pr-6 mr-4 h6">
+                            <li><a class="text-muted" href="#">Action</a></li>
+                            <li><a class="text-muted" href="#">Adventure</a></li>
+                            <li><a class="text-muted" href="#">Detective</a></li>
+                            <li><a class="text-muted" href="#">Mystery</a></li>
+                            <li><a class="text-muted" href="#">Horror</a></li>
+                            <li><a class="text-muted" href="#">Romance</a></li>
+                            <li><a class="text-muted" href="#">Sci-Fi</a></li>
+                        </ul>
+                        <ul class="h6">
+                            <li><a class="text-muted" href="#">Art</a></li>
+                            <li><a class="text-muted" href="#">Biography</a></li>
+                            <li><a class="text-muted" href="#">Children's</a></li>
+                            <li><a class="text-muted" href="#">History</a></li>
+                            <li><a class="text-muted" href="#">Music</a></li>
+                            <li><a class="text-muted" href="#">Self Help</a></li>
+                            <li><a class="text-muted" href="#">Travel</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <!-- Authors Links -->
+                <div class=" mx-auto col-3">
+                    <div class="border_color_1 mb-2">
+                        <p class="text_color_1 font-weight-bold h4">AUTHORS</p>
+                    </div>
+                    <div class="category_links d-flex justify-content-start">
+                        <ul class="pr-6 mr-4 h6">
+                            <li><a class="text-muted" href="#">Stephen King</a></li>
+                            <li><a class="text-muted" href="#">Rachel Poiners</a></li>
+                            <li><a class="text-muted" href="#">Nikola Tesla</a></li>
+                            <li><a class="text-muted" href="#">Leonardo Da Vinci</a></li>
+                            <li><a class="text-muted" href="#">Michael Lewis</a></li>
+                            <li><a class="text-muted" href="#">James Chance</a></li>
+                            <li><a class="text-muted" href="#">Jim Kwik</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="d-flex justify-content-between align-items-center text-muted py-4 border-top">
+                <p class="h6 text-muted">Copyright&copy; 2015 Bookix.com All Rights Reserved. Designed by Zil Norvilis,
+                    idea by
+                    Mohammed Awad
+                </p>
+                <div class="social">
+                    <a href="#"><i class="fab fa-facebook"></i></a>
+                    <a href="#"><i class="fab fa-twitter"></i></a>
+                    <a href="#"><i class="fab fa-instagram"></i></a>
+                    <a href="#"><i class="fab fa-linkedin"></i></a>
+                </div>
+            </div>
+        </div>
+    </footer>
+`;
+
+document.write(page);
